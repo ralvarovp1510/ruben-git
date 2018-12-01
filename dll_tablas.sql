6d2fa07a2a138258bb09692ceb6f6992f95561c5
ruben <rubenpucp123@gmail.com>
2018-12-01 10:39:16 -0500

    nuevo cambio

--- dll_tablas.sql
+++ dll_tablas.sql
@@ -3,4 +3,8 @@ muchas tablaste
 --tabla de clientes
 create table clientes ( dni char(10),
 nombre varchar(30), apellidos varchar(30),
-direccion varchar(100))
+direccion varchar(100)
+
+--insertamos valores a tabla
+insert into cñientes values ("45853164", "pepito","juarez","av. carrion #1234"  )
+
